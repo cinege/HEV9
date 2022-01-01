d49cb07a66f95e6065c0704cbc305ec3375b5348
Tamas Nandori <tamas.nandori@gmail.com>
2022-01-01 09:20:05 +0100

    Code CleanUp

--- app/src/main/java/com/example/hev_9/MainActivity.kt
+++ app/src/main/java/com/example/hev_9/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.hev_9
 
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -22,18 +21,10 @@ import javax.net.ssl.HttpsURLConnection
 // View model
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import java.net.InetSocketAddress
-import java.net.Socket
-import android.net.NetworkInfo
-
-import android.net.ConnectivityManager
-
-
-
 
 class MainActivity : AppCompatActivity() {
-    private val OrsID = "BKK_004903"
-    private val ArpadfoldID = "BKK_F03409"
+    private val orsId = "BKK_004903"
+    private val arpadfoldId = "BKK_F03409"
     private var textViewFirst: TextView? = null
     private var textViewSecond: TextView? = null
     private var buttonFetch: Button? = null
@@ -54,8 +45,8 @@ class MainActivity : AppCompatActivity() {
         textViewFourth = findViewById(R.id.textview_fourth)
         buttonFetch2 = findViewById(R.id.button_fetch2)
 
-        buttonFetch?.setOnClickListener({fetch(OrsID,"stopHeadsign", "Csömör")})
-        buttonFetch2?.setOnClickListener({fetch(ArpadfoldID, "stopHeadsign","Örs vezér tere")})
+        buttonFetch?.setOnClickListener {fetch(orsId, "stopHeadsign", "Csömör") }
+        buttonFetch2?.setOnClickListener{fetch(arpadfoldId, "stopHeadsign","Örs vezér tere")}
 
         viewModel.first.observe(this, {textViewFirst?.text = it})
         viewModel.second.observe(this, {textViewSecond?.text = it})
@@ -65,47 +56,39 @@ class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun fetch(stopID: String, attr: String, value: String) {
-        if (true) {
-            var departures: Departures?
-            lifecycleScope.launch(Dispatchers.IO) {
-                try {
-                    val result = getRequest(stopID)
-                    departures = result?.let { Departures(it, attr, value) }
-                    withContext(Dispatchers.Main) {
-                        when (stopID) {
-                            //Ors
-                            "BKK_004903" -> {
-                                viewModel.first.value = departures?.first
-                                viewModel.second.value = departures?.second
-                            }
-                            //Arpadfold
-                            "BKK_F03409" -> {
-                                viewModel.third.value = departures?.first
-                                viewModel.fourth.value = departures?.second
-                            }
+        var departures: Departures?
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val result = getRequest(stopID)
+                departures = Departures(result, attr, value)
+                withContext(Dispatchers.Main) {
+                    when (stopID) {
+                        //Ors
+                        "BKK_004903" -> {
+                            viewModel.first.value = departures?.first
+                            viewModel.second.value = departures?.second
+                        }
+                        //Arpadfold
+                        "BKK_F03409" -> {
+                            viewModel.third.value = departures?.first
+                            viewModel.fourth.value = departures?.second
                         }
                     }
-                } catch (e: Exception) {
-                    println("Connection Error")
                 }
+            } catch (e: Exception) {
+                println("Connection Error")
             }
         }
     }
 
-    private fun getRequest(stopID: String): String? {
-
+    private fun getRequest(stopID: String): String {
         val inputStream: InputStream
-        var result = "{}"
-
-        val sURL_part1 ="https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId="
-        val sURL_part2 ="&minutesBefore=1&minutesAfter=120"
-        val url = URL(sURL_part1 + stopID + sURL_part2)
+        val sURLpart1 ="https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId="
+        val sURLpart2 ="&minutesBefore=1&minutesAfter=120"
+        val url = URL(sURLpart1 + stopID + sURLpart2)
         val conn: HttpsURLConnection = url.openConnection() as HttpsURLConnection
         conn.connect()
         inputStream = conn.inputStream
-        result = inputStream?.bufferedReader()?.use(BufferedReader::readText) ?: "error: inputStream is null"
-        return result
+        return inputStream?.bufferedReader()?.use(BufferedReader::readText) ?: "error: inputStream is null"
     }
-
-
 }
